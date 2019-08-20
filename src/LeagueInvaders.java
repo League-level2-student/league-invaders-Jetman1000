ffe8a9f38a927709b6440734ea23d7d6c0f62211
ilabstaff <ilabstaff@172.16.116.73>
2019-08-19 18:00:40 -0700

    GamePanel.java, LeagueInvaders.java

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,27 @@
-
+import javax.swing.JFrame;
+// you left off on the VIEW part of the module (ADD Text)
 public class LeagueInvaders {
-
+	JFrame frame;
+	GamePanel GP;
+	public static final int WIDTH=500;
+	public static final int HEIGHT=800;
+	
+	LeagueInvaders(){
+		frame= new JFrame();
+		GP=new GamePanel();
+	}
+	
+	public static void main(String[] args) {
+		LeagueInvaders LI= new LeagueInvaders();
+		LI.setup();
+	}
+	
+	void setup() {
+		frame.add(GP);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
 }
+
